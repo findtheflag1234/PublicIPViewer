9cf1917048f20067a39ccd684bcc79a55dbd990e
findtheflag1234 <houet.hubert@gmail.com>
2024-11-28 22:03:14 +0100

    Update publicipviewer.py

--- publicipviewer.py
+++ publicipviewer.py
@@ -1,1 +1,16 @@
+import requests
 
+def get_public_ip():
+    # Utilisation de l'API gratuite "https://api.ipify.org" pour récupérer l'IP publique
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        if response.status_code == 200:
+            ip = response.json()['ip']
+            print(f"Votre adresse IP publique est : {ip}")
+        else:
+            print("Erreur lors de la récupération de l'adresse IP.")
+    except requests.exceptions.RequestException as e:
+        print(f"Une erreur est survenue : {e}")
+
+if _name_ == "_main_":
+    get_public_ip()
